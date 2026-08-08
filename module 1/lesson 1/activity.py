1dc2764260d75236c66590c65bf0b2c1bf5d6bc9
Miftahul04 <ranisomc37@gmail.com>
2026-08-08 10:56:40 +0600

    first lesson

---
+++ module 1/lesson 1/activity.py
@@ -0,0 +1,5 @@
+print("welcome to python programming")
+#This is comment
+print("my name is Sidratul. Today is my first class.")
+#\n new line
+print("I am very excited.\nI hope to learn new things.")
