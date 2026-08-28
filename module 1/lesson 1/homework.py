0c1dd63cc864d2a216ab48e9e5285c7f1993e354
Miftahul04 <ranisomc37@gmail.com>
2026-08-28 10:08:00 +0600

    lesson 1 homework

---
+++ module 1/lesson 1/homework.py
@@ -0,0 +1,24 @@
+# Printing the title
+print("My Daily Routine")
+# Step 1: Waking up
+print("I wake up at 7 am")
+# Step 2: Brushing my teeth
+print("I brush my teeth at 7:10 am")
+# Step 3: Having breakfast
+print("I eat breakfast at 7:30 am")
+# Step 4: Head out
+print(""I go to school at 8:00 am)
+# Step 5: Unpack and unwind
+print("I come back from school at 2:00 pm")
+# Step 6: Having lunch
+print("I have lunch at 3:00 pm")
+# Step 7: Taking a nap
+print("I take a nap at 3:30 pm")
+# Step 8: Playing
+print("I play with my friends at 4:30 pm")
+# Step 9: Study time
+print("I do my school work at 6:30 pm")
+# Step 10: Having dinner
+print("I have dinner at 8:30 pm")
+# Step 11: Going to bed
+print("I go to bed at 10:00 pm")
