b0ba41f0b094beaa8d27bf9145dd9c6d8a06359b
Miftahul04 <ranisomc37@gmail.com>
2026-08-28 10:45:49 +0600

    lesson 3 activity.py1

---
+++ module 1/lesson 3/activity1.py
@@ -0,0 +1,13 @@
+age = 13
+Total_marks = 355.67
+name = "Sarah"
+is_student = True
+#Printing the data type of the value
+print(type(age))
+print(type(Total_marks))
+print(type(name))
+print(type(is_student))
+#typecasting -> converting a value into of one data type into another data type
+Total_marks = int(Total_marks)
+print(Total_marks)
+print(type(Total_marks))
